5eb751d30278b46e8a5ca84f771e0a6fe72ec4cd
Robin Andersson <robin.eric.andersson@gmail.com>
2019-06-29 00:08:37 +0200

    fix

--- hops/distribute/allreduce.py
+++ hops/distribute/allreduce.py
@@ -134,10 +134,11 @@ def _prepare_func(app_id, run_id, map_fun, local_logdir, server_addr, eval_node)
 
             chief = cluster["cluster"]["worker"].pop(len(cluster["cluster"]["worker"]) -1)
             cluster["cluster"]["chief"] = [chief]
-            if chief == host_port:
-                cluster["task"] = {"type": "chief", "index": 0}
-            else:
-                cluster["task"] = {"type": "worker", "index": task_index}
+            if evaluator == None:
+                if chief == host_port:
+                    cluster["task"] = {"type": "chief", "index": 0}
+                else:
+                    cluster["task"] = {"type": "worker", "index": task_index}
 
             print(cluster)
 
