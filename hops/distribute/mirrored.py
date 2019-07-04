248572999d8d4e6c6afc00ebcfa854c7139f41fb
Robin Andersson <robin.eric.andersson@gmail.com>
2019-07-04 14:17:51 +0200

    add evaluator

--- hops/distribute/mirrored.py
+++ hops/distribute/mirrored.py
@@ -19,7 +19,7 @@ import json
 from . import allreduce_reservation
 
 
-def _launch(sc, map_fun, run_id, local_logdir=False, name="no-name"):
+def _launch(sc, map_fun, run_id, local_logdir=False, name="no-name", evaluator=False):
     """
 
     Args:
@@ -45,7 +45,7 @@ def _launch(sc, map_fun, run_id, local_logdir=False, name="no-name"):
     server_addr = server.start()
 
     #Force execution on executor, since GPU is located on executor
-    nodeRDD.foreachPartition(_prepare_func(app_id, run_id, map_fun, local_logdir, server_addr))
+    nodeRDD.foreachPartition(_prepare_func(app_id, run_id, map_fun, local_logdir, server_addr, evaluator))
 
     logdir = _get_logdir(app_id, run_id)
 
@@ -71,7 +71,7 @@ def _get_logdir(app_id, run_id):
     """
     return util._get_experiments_dir() + '/' + app_id + '_' + str(run_id)
 
-def _prepare_func(app_id, run_id, map_fun, local_logdir, server_addr):
+def _prepare_func(app_id, run_id, map_fun, local_logdir, server_addr, evaluator):
     """
 
     Args:
@@ -122,6 +122,13 @@ def _prepare_func(app_id, run_id, map_fun, local_logdir, server_addr):
                 cluster["task"] = {"type": "chief", "index": 0}
             else:
                 cluster["task"] = {"type": "worker", "index": task_index}
+
+            if evaluator:
+                evaluator_node = cluster["cluster"]["worker"][0]
+                cluster["cluster"]["evaluator"] = [evaluator_node]
+                del cluster["cluster"]["worker"][0]
+                if evaluator_node == host_port:
+                    cluster["task"] = {"type": "evaluator", "index": 0}
 
             print('TF_CONFIG: {} '.format(cluster))
 
