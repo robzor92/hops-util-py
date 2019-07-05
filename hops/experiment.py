e115f418e1f6bac3db2e4bb377d7055fd88af120
Robin Andersson <robin.eric.andersson@gmail.com>
2019-07-05 11:17:11 +0200

    fix

--- hops/experiment.py
+++ hops/experiment.py
@@ -90,12 +90,14 @@ def launch(map_fun, args_dict=None, name='no-name', local_logdir=False, versione
 
         util._publish_experiment(app_id, run_id, experiment_json, 'CREATE')
 
-        retval, tensorboard_logdir, hp = launcher._launch(sc, map_fun, run_id, args_dict, local_logdir)
 
-        if retval:
-            experiment_json = util._finalize_experiment(experiment_json, hp, retval)
-            util._publish_experiment(app_id, run_id, experiment_json, 'REPLACE')
-            return tensorboard_logdir
+        start = time.time()
+        retval, tensorboard_logdir, hp = launcher._launch(sc, map_fun, run_id, args_dict, local_logdir)
+        duration = time.time() - start
+
+        experiment_json = util._finalize_experiment(experiment_json, hp, retval, duration)
+        util._publish_experiment(app_id, run_id, experiment_json, 'REPLACE')
+        return tensorboard_logdir
     except:
         _exception_handler()
         raise
@@ -104,8 +106,6 @@ def launch(map_fun, args_dict=None, name='no-name', local_logdir=False, versione
         run_id +=1
         running = False
         sc.setJobGroup("", "")
-    return tensorboard_logdir
-
 
 def random_search(map_fun, boundary_dict, direction='max', samples=10, name='no-name', local_logdir=False, versioned_resources=None, description=None):
     """
@@ -606,7 +606,6 @@ def _setup_experiment(versioned_resources, logdir, app_id, run_id):
     hopshdfs.mkdir(hopshdfs.get_plain_path(util._get_experiments_dir()) + "/" + app_id + "_" + str(run_id))
     return versioned_path
 
-def _finalize_experiment(experiment_json, hp, app_id,_run_id):
-    experiment_json = util._finalize_experiment(experiment_json, hp, None)
-
+def _finalize_experiment(experiment_json, hp, app_id,_run_id, duration):
+    experiment_json = util._finalize_experiment(experiment_json, hp, None, 'FINISHED', duration)
     util._publish_experiment(app_id, run_id, experiment_json, 'REPLACE')
