a5cbd22266e9ca6317ff248b61c374ee8fe9664c
Robin Andersson <robin.eric.andersson@gmail.com>
2019-07-18 16:57:36 +0200

    fix

--- hops/experiment_impl/distribute/allreduce.py
+++ hops/experiment_impl/distribute/allreduce.py
@@ -143,7 +143,7 @@ def _prepare_func(app_id, run_id, map_fun, local_logdir, server_addr, evaluator,
             retval = map_fun()
             if is_chief:
                 if retval:
-                    _handle_return(retval, logdir)
+                    experiment_utils._handle_return(retval, logdir)
             task_end = time.time()
             time_str = 'Finished task - took ' + experiment_utils._time_diff(task_start, task_end)
             print('\n' + time_str)
