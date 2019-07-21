bcbf0436e647b218bd17b0e2c0a08984e72e1f9e
Robin Andersson <robin.eric.andersson@gmail.com>
2019-07-21 13:21:32 +0200

    fix

--- hops/experiment_impl/util/experiment_utils.py
+++ hops/experiment_impl/util/experiment_utils.py
@@ -38,7 +38,7 @@ def _handle_return(retval, hdfs_exec_logdir, optimization_key):
     # Validation
     if not optimization_key and type(retval) is dict and len(retval.keys()) > 1:
         raise Exception('Missing optimization_key argument, when returning multiple values in a dict the optimization_key argument must be set.')
-    elif type(retval) is dict and optimization_key not in retval:
+    elif type(retval) is dict and optimization_key not in retval and len(retval.keys()) > 1:
         raise Exception('Specified optimization key {} not in returned dict'.format(optimization_key))
     elif type(retval) is dict and retval(retval.keys()) == 0:
         raise Exception('Returned dict is empty, must contain atleast 1 metric to maximize or minimize.')
