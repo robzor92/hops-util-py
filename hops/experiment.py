34bc2b4dd7dad10dd51b71616d6cdbd50fac1fcd
Robin Andersson <robin.eric.andersson@gmail.com>
2019-08-02 11:39:43 +0200

    fix

--- hops/experiment.py
+++ hops/experiment.py
@@ -165,7 +165,7 @@ def random_search(map_fun, boundary_dict, direction='max', samples=10, name='no-
         logdir, best_param, best_metric = r_search_impl._run(sc, map_fun, run_id, boundary_dict, samples, direction=direction, local_logdir=local_logdir, optimization_key=optimization_key)
         duration = experiment_utils._microseconds_to_millis(time.time() - start)
 
-        experiment_utils._finalize_experiment(experiment_json, best_param, best_metric, app_id, run_id, 'FINISHED', duration, logdir)
+        experiment_utils._finalize_experiment(experiment_json, best_param, best_metric, app_id, run_id, 'FINISHED', duration, experiment_utils._get_logdir(app_id, run_id))
 
         best_param_dict = experiment_utils._convert_to_dict(best_param)
 
@@ -245,7 +245,7 @@ def differential_evolution(objective_function, boundary_dict, direction = 'max',
         logdir, best_param, best_metric = diff_evo_impl._run(objective_function, boundary_dict, direction=direction, generations=generations, popsize=population, mutation=mutation, crossover=crossover, cleanup_generations=cleanup_generations, local_logdir=local_logdir, name=name, optimization_key=optimization_key)
         duration = experiment_utils._microseconds_to_millis(time.time() - start)
 
-        experiment_utils._finalize_experiment(experiment_json, best_param, best_metric, app_id, run_id, 'FINISHED', duration, logdir)
+        experiment_utils._finalize_experiment(experiment_json, best_param, best_metric, app_id, run_id, 'FINISHED', duration, experiment_utils._get_logdir(app_id, run_id))
 
         best_param_dict = experiment_utils._convert_to_dict(best_param)
 
@@ -328,7 +328,7 @@ def grid_search(map_fun, args_dict, direction='max', name='no-name', local_logdi
         logdir, best_param, best_metric = grid_search_impl._run(sc, map_fun, run_id, grid_params, direction=direction, local_logdir=local_logdir, name=name, optimization_key=optimization_key)
         duration = experiment_utils._microseconds_to_millis(time.time() - start)
 
-        experiment_utils._finalize_experiment(experiment_json, best_param, best_metric, app_id, run_id, 'FINISHED', duration, logdir)
+        experiment_utils._finalize_experiment(experiment_json, best_param, best_metric, app_id, run_id, 'FINISHED', duration, experiment_utils._get_logdir(app_id, run_id))
 
         best_param_dict = experiment_utils._convert_to_dict(best_param)
 
