b9ac4c0291a6a5ce52bed418fe67c791d7ad8d03
Robin Andersson <robin.eric.andersson@gmail.com>
2019-08-12 12:44:30 +0200

    fix

--- hops/experiment_impl/launcher.py
+++ hops/experiment_impl/launcher.py
@@ -52,7 +52,7 @@ def _run(sc, map_fun, run_id, args_dict=None, local_logdir=False, name="no-name"
         path_to_return = experiment_utils._get_logdir(app_id, run_id) + '/.return'
         if hdfs.exists(path_to_return):
             contents = json.loads(hdfs.load(path_to_return))
-            return experiment_utils._get_logdir(app_id, run_id), None, json.dumps(contents)
+            return experiment_utils._get_logdir(app_id, run_id), None, contents
     elif num_executions == 1 and not args_dict == None:
         arg_count = six.get_function_code(map_fun).co_argcount
         arg_names = six.get_function_code(map_fun).co_varnames
@@ -66,11 +66,9 @@ def _run(sc, map_fun, run_id, args_dict=None, local_logdir=False, name="no-name"
             argIndex += 1
         param_string = param_string[:-1]
         path_to_metric = experiment_utils._get_logdir(app_id, run_id) + '/' + param_string + '/.return'
-        if pydoop.hdfs.path.exists(path_to_metric):
-            with pydoop.hdfs.open(path_to_metric, "r") as fi:
-                metric = float(fi.read())
-                fi.close()
-                return experiment_utils._get_logdir(app_id, run_id), param_string, metric
+        if hdfs.exists(path_to_return):
+            contents = json.loads(hdfs.load(path_to_return))
+            return experiment_utils._get_logdir(app_id, run_id), param_string, contents
         else:
             return experiment_utils._get_logdir(app_id, run_id), param_string, None
 
